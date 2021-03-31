c4d9e1230138089445b33e807bfba7bd6f242020
Uugne <ugne.kurkyte@gmail.com>
2021-03-31 16:34:04 +0300

    switch

---
+++ js/08-switch.js
@@ -0,0 +1,63 @@
+const day = 1;
+
+switch (day) {
+    case 1:
+        console.log('pirmadienis');
+        break;
+
+    case 2:
+        console.log('antradienis');
+        break;
+            
+    case 3:
+        console.log('treciadienis');
+        break;
+
+    default:
+        console.log('Nezinau tokios dienos');
+        break;
+}
+
+const day2 = 1;
+
+switch (day2) {
+    case 1:
+        console.log('Darbo diena');
+        break;
+}
+
+// procesas:
+// - paimti puodeli
+// - ideti arbatos pakeli
+// - isideti cukraus
+// - ipilti vandens
+// - ismaisyti
+// - gerti
+
+const processStep = 1;
+
+switch (processStep) {
+    case 1:
+    console.log('Paimti puodeli');
+    
+
+    case 2:
+    console.log('Ideti arbatos pakeli');
+    
+
+    case 3:
+    console.log('3');
+    
+
+    case 4:
+    console.log('4');
+    
+
+    case 5:
+    console.log('5');
+    
+
+    case 6:
+    console.log('6');
+    break;
+}
